39751e936bf4914c65aaf9249bfc8fa5c355dffc
土井 正宏 <doi_masahiro@cyberagent.co.jp>
2019-08-30 12:53:48 +0900

    横幅変更

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
-    width: 800, height: 600,
+    width: 950, height: 600,
     webPreferences: {
       nodeIntegration: false
     }
